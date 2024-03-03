e5faeba1adceedf579e2667376b8e0aff69d6332
Huma Prathama <huma.elektro@gmail.com>
2024-03-03 22:12:22 +0800

    change layout

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Unit 2: Flutter Widget'),
+      home: const MyHomePage(title: 'Unit 2: Profile Pengguna'),
     );
   }
 }
@@ -31,21 +31,53 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          Image.asset("images/banner.jpeg"),
+          Image.network(
+              "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.copy),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.share),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.report),
+              ),
+            ],
+          ),
           const ListTile(
             title: Text("Wayan Flutter"),
-            subtitle: Text("Program Studi Teknologi Informasi"),
+            subtitle: Text("Nama"),
           ),
           const ListTile(
-            title: Text("Made Dart"),
-            subtitle: Text("Program Studi Teknik Elektro"),
+            title: Text("Teknologi Informasi"),
+            subtitle: Text("Program Studi"),
           ),
           const ListTile(
-            title: Text("Komang Kotlin"),
-            subtitle: Text("Program Studi Teknik Sipil"),
-          )
+            title: Text("Teknologi Informasi"),
+            subtitle: Text("Program Studi"),
+          ),
+          const ListTile(
+            title: Text("923010331"),
+            subtitle: Text("Nomor Induk Mahasiswa"),
+          ),
+          const ListTile(
+            title: Text(
+                "Jalan Gatot Subroto I no 15 Denpasar Utara, Denpasar, Bali"),
+            subtitle: Text("Alamat"),
+          ),
+          const ListTile(
+            title: Text(
+                "Est ullamco deserunt sunt aliqua eiusmod dolor nulla. Dolor consectetur ad exercitation irure reprehenderit Lorem. Eu magna labore excepteur cupidatat amet consectetur esse occaecat aliquip nulla officia. Elit eu id eu reprehenderit aute laborum cillum. Reprehenderit sit velit labore et nostrud nisi consequat in ut laboris mollit. Esse non proident sint pariatur sit officia est consectetur id labore consectetur. Lorem consequat nostrud mollit irure mollit ea nulla labore mollit."),
+            subtitle: Text("Deskripsi"),
+          ),
         ],
       ),
     );
